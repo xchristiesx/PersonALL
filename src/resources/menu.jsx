670ec55f3efaaddfd6a6a3f165f948bd87703554
Adéla Fialová <adela.grnd@gmail.com>
2023-12-03 15:20:44 +0100

    add missing menu file for the header

---
+++ src/resources/menu.jsx
@@ -0,0 +1,14 @@
+export const menu = [
+    {
+        menuTitle: "O nás",
+        menuLink: "/about"
+    },
+    {
+        menuTitle: "Test",
+        menuLink: "/test"
+    },
+    // {
+    //     menuTitle: "Kontakty",
+    //     menuLink:
+    // }
+];
